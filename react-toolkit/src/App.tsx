7a42c2874aac37d69280743b7781d2d6c2f49f8e
Mustafa <makman09@gmail.com>
2023-12-25 19:47:33 -0800

    slightly collapsible sidebar

--- react-toolkit/src/App.tsx
+++ react-toolkit/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import HelloWorld from './components/HelloWorld/HelloWorld';
@@ -6,11 +6,15 @@ import Example from './components/Example/Example';
 import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
   return (
     <div className='App'>
       <Router>
-        <Sidebar/>
+        <Sidebar open={sidebarOpen}/>
         <div className="wrapper">
+          <div className="vl" onClick={() => {
+            setSidebarOpen(!sidebarOpen);
+          }}></div>
           <Routes>
             <Route path="/" element={<HelloWorld />} />
             <Route path="/example" element={<Example />} />
--- react-toolkit/src/components/Sidebar/Sidebar.tsx
+++ react-toolkit/src/components/Sidebar/Sidebar.tsx
@@ -4,18 +4,10 @@ import styles from './Sidebar.module.scss';
 
 interface SidebarProps {
   open?: boolean;
-  onToggle?: () => void;
 }
 
-const Sidebar: FC<SidebarProps> = ({ open = false, onToggle }) => {
+const Sidebar: FC<SidebarProps> = ({ open = false }) => {
   const [sidebarOpen, setSidebarOpen] = useState(open);
-
-  const toggleSidebar = () => {
-    setSidebarOpen(!sidebarOpen);
-    if (onToggle) {
-      onToggle();
-    }
-  };
 
   useEffect(() => {
     // Sync state with parent component if provided
@@ -25,17 +17,21 @@ const Sidebar: FC<SidebarProps> = ({ open = false, onToggle }) => {
   }, [open]);
 
   return (
-  <div className={styles.Sidebar} data-testid="Sidebar">
-    <h2>React Toolkit</h2>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-        <Link to="/example">Example</Link>
-      </li>
-      {/* Add more links as needed */}
-    </ul>
-  </div>
-  )
+    <div
+      className={styles.Sidebar}
+      data-testid="Sidebar"
+      style={open ? { flex: "0 0 250px", width: "250px", } : { flex: "0", width: "0px" }}
+    >
+      <h2>React Toolkit</h2>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+          <Link to="/example">Example</Link>
+        </li>
+        {/* Add more links as needed */}
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
