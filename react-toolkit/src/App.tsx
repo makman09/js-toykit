e9c0ee6cee1ef24800ac90de476be25900020a0f
Mustafa <makman09@gmail.com>
2023-12-27 18:08:10 -0800

    good save point

--- react-toolkit/src/App.tsx
+++ react-toolkit/src/App.tsx
@@ -1,17 +1,27 @@
-import React, { useState } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './App.scss';
 import HelloWorld from './components/HelloWorld/HelloWorld';
 import Example from './components/Example/Example';
-import Sidebar from './components/Sidebar/Sidebar';
+// import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   const [sidebarOpen, setSidebarOpen] = useState(true);
   return (
     <div className='App'>
       <Router>
-        <Sidebar open={sidebarOpen}/>
-        <div className="wrapper">
+          <div className="sidebar">
+            <h2>React Toolkit</h2>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+                <Link to="/example">Example</Link>
+              </li>
+              {/* Add more links as needed */}
+            </ul>
+          </div>
+         <div className="wrapper">
           <div className="vl" onClick={() => {
             setSidebarOpen(!sidebarOpen);
           }}></div>
